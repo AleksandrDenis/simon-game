e1ea8589578dee9c803c018913be80ff47d0a244
DenisKudriavcev <deniskudriavcev@gmail.com>
2022-07-28 21:03:34 +0000

    Add test

---
+++ scripts/game.js
@@ -0,0 +1,11 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+};
+function newGame() {
+    game.score = 0;
+}
+
+module.exports = { game, newGame };
